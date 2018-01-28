6aedf0b81abc4d6f26cef686f487547724541a1f
Quien Sabe <qs5779@mail.com>
2018-01-28 11:29:54 -0500

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# nixadmutils
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include nixadmutils
+class nixadmutils {
+}
---
+++ manifests/install.pp
@@ -0,0 +1,10 @@
+# nixadmutils::install
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include nixadmutils::install
+class nixadmutils::install {
+}

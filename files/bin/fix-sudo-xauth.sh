5c468d83389e7948385eb047e406e7b61b275a1c
Quien Sabe <quiensabe@wtfo-guru.com>
2019-01-29 08:44:09 -0500

    added fix-sudo-xauth update fw-list

---
+++ files/bin/fix-sudo-xauth.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DISPLAY_NUMBER=$(echo $DISPLAY | awk -F: '{print $2}' | awk -F. '{print $1}')
+EC=0
+
+if [ -n "$DISPLAY_NUMBER" ]
+then
+  #echo "DISPLAY #: $DISPLAY_NUMBER"
+  COOKIE=$(xauth list | grep "unix:${DISPLAY_NUMBER}")
+  #echo "COOKIE: $COOKIE"
+  if [ -n "$COOKIE" ]
+  then
+    echo "executing: sudo xauth add \"$COOKIE\""
+    sudo xauth add "$COOKIE"
+  else
+    echo "No cookie found!"
+    EC=1
+  fi
+else
+  echo "No display number found!"
+  EC=1
+fi
+
+exit $EC
+

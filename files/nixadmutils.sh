a06b9f3f172f65992dd81be01ba360e56c865f86
Quien Sabe <qs5779@mail.com>
2019-04-25 12:04:23 -0400

    path fix

--- files/nixadmutils.sh
+++ files/nixadmutils.sh
@@ -1,10 +1,20 @@
 # Add /opt/nixadmutils/bin to the path for sh compatible users
 
-for d in bin sbin build/bin
+for d in bin build/bin
 do
   if ! echo $PATH | grep -q /opt/nixadmutils/${d} ; then
     PATH=$PATH:/opt/nixadmutils/${d}
   fi
 done
+
+if groups | grep -q -e wheel -e sudo
+then
+  for d in /opt/nixadmutils/sbin /usr/local/sbin
+  do
+    if ! echo $PATH | grep -q ${d} ; then
+      PATH=$PATH:${d}
+    fi
+  done
+fi
 
 export PATH

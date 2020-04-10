c8d6b4a8d4a2298ed48e7c3ecca3027f71f98ff1
Quien Sabe <qs5779@mail.com>
2020-04-10 11:55:50 -0400

    Updated pdk version. Verified rubocop passing. Increase spec coverage.

--- spec/classes/nixadmutils_spec.rb
+++ spec/classes/nixadmutils_spec.rb
@@ -13,6 +13,22 @@ describe 'nixadmutils' do
       it { is_expected.to contain_file('/opt/nixadmutils/sbin') }
       it { is_expected.to contain_file('/etc/profile.d/nixadmutils.csh') }
       it { is_expected.to contain_file('/etc/profile.d/nixadmutils.sh') }
+      it { is_expected.to contain_file('/opt/nixadmutils/bin/fw-list') }
+      it { is_expected.to contain_file('/opt/nixadmutils/bin/pkglist') }
+      it { is_expected.to contain_file('/opt/nixadmutils/build') }
+      it { is_expected.to contain_file('/opt/nixadmutils/sbin/findpkg') }
+      it { is_expected.to contain_file('/opt/nixadmutils/sbin/installpkg') }
+      it { is_expected.to contain_file('/opt/nixadmutils/sbin/listpkgs') }
+      it { is_expected.to contain_file('/opt/nixadmutils/sbin/lspuppet') }
+      it { is_expected.to contain_file('/opt/nixadmutils/sbin/pacwrap') }
+      it { is_expected.to contain_file('/opt/nixadmutils/sbin/pkgfiles') }
+      it { is_expected.to contain_file('/opt/nixadmutils/sbin/pkglist') }
+      it { is_expected.to contain_exec('/opt/nixadmutils/bin') }
+      it { is_expected.to contain_exec('/opt/nixadmutils/build') }
+      it { is_expected.to contain_exec('/opt/nixadmutils/sbin') }
+      it { is_expected.to contain_class('Nixadmutils::Config') }
+      it { is_expected.to contain_class('Nixadmutils::Install') }
+      it { is_expected.to contain_class('Nixadmutils::Params') }
     end
   end
 end

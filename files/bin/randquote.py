b3ef53ac4be2d4977ab858c6bbd80c4d1e92b39d
Quien Sabe <qs5779@mail.com>
2021-12-23 15:33:26 -0400

    Improved randquotes script.

--- files/bin/randquote.py
+++ files/bin/randquote.py
@@ -4,11 +4,14 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
 import glob
+import hashlib
 import json
 import logging
 from optparse import OptionParser
 import os
 from pathlib import Path
+# import pprint
+from random import randrange
 import re
 import requests
 import shutil
@@ -55,6 +58,12 @@ class QuoteServer():
 
     except Exception as e:
       raise
+
+  def __yes_or_no(self, question):
+    reply = str(input(question+' (Y/n): ')).lower().strip()
+    if (reply == '') or (reply[0] == 'y'):
+        return True
+    return False
 
   def __today(self):
     return datetime.now().strftime('%Y%m%d')
@@ -207,6 +216,42 @@ class QuoteServer():
       last = 0
     return int(self.__today()) > int(last)
 
+  def __scrape_inspiringquotes(self, url, author, category):
+    md5sum = hashlib.md5(url.encode('utf-8')).hexdigest()
+    cache = Path("/tmp/%s.html" % md5sum)
+    quotes = {}
+    if cache.exists():
+      with open(cache, 'r') as f:
+        response_data = f.read()
+    else:
+      response_data = requests.get(url).text[:]
+      with open(cache, 'w') as f:
+        f.write(response_data)
+    soup = BeautifulSoup(response_data, 'html.parser')
+    for bq in soup.find_all("blockquote"):
+      # sbq = str(bq)
+      # print(sbq)
+      e_quote = bq.get_text().strip()
+      em = bq.find("em")
+      if em:
+        e_author = em.get_text().strip()
+        e_quote = e_quote.replace(e_author, '')
+      else:
+        e_author = author
+
+      e_quote = e_quote.replace(' / ', '')
+      if e_author:
+        e_author = re.sub(r",.*", '', e_author)
+        e_author = re.sub(r"^[^A-Za-z]+", '', e_author)
+      if e_quote in quotes:
+        quotes[e_quote]['count'] += 1
+      else:
+        quotes[e_quote] = {}
+        quotes[e_quote]['count'] = 1
+        quotes[e_quote]['author'] = e_author
+        quotes[e_quote]['category'] = category
+    return quotes
+
   def __scrape_brainy_qod(self):
     url = 'https://www.brainyquote.com/quote_of_the_day'
     try:
@@ -244,8 +289,94 @@ class QuoteServer():
           failed += 1
           continue
         self.__add_quote(tauthor, tquote, '0', tcategory)
+        scraped += 1
+      if (sys.stdout.isatty()) and (scraped > 0):
+        print("Added %d brainy quotes of the day to database." % scraped)
     except Exception as e:
       self.__add_error('scrape_brainy_qod: %s' % str(e))
+
+  def parse_url(self, url, author, category):
+    if not author:
+      author = 'unknown'
+    if not category:
+      category = 'None'
+    if not re.match('^http[s]*://', url):
+      url = 'https://' + url
+    if self.options['debug']:
+      print('url: %s' % url)
+      print('author: %s' % author)
+      print('category: %s' % category)
+    if re.search('inspiringquotes.com', url):
+      quotes = self.__scrape_inspiringquotes(url, author, category)
+    else:
+      match = re.search(r"^http[s]*://([^/]+)", url)
+      if match:
+        tgt = match.group(1)
+      else:
+        tgt = url
+      print("Unrecognized site: %s" % tgt)
+      return 0
+    extracted = len(quotes)
+    if extracted > 0:
+      for qq in quotes:
+        print('''quote: "%s"''' % qq)
+        print('''author: "%s"''' % quotes[qq]['author'])
+        print('''category: "%s"''' % quotes[qq]['category'])
+        print('count: %d' % quotes[qq]['count'])
+      answer = self.__yes_or_no("Import the quotes?")
+      if answer:
+        qnbr = 0
+        self.__connect()
+        for qq in quotes:
+          self.__add_quote(quotes[qq]['author'], qq, 0, quotes[qq]['category'])
+          qnbr += 1
+        print("Added %d quotes to database." % qnbr)
+
+  def __get_random_never_used(self, cursor):
+
+    query = '''SELECT COUNT(*) FROM quotes WHERE quotes.used < 1'''
+    cursor.execute(query)
+    crow = cursor.fetchone()
+    numrows = crow[0]
+    if numrows > 0:
+      query = '''SELECT
+                    quotes.quote_id AS qid,
+                    quotes.quote AS quote,
+                    authors.author_name AS author,
+                    categories.category AS category,
+                    quotes.used AS used
+                  FROM quotes
+                  LEFT JOIN authors ON quotes.author_id = authors.author_id
+                  LEFT JOIN categories ON quotes.category_id = categories.category_id
+                  WHERE quotes.used < 1;
+                    '''
+      randrow = randrange(0, numrows)
+      if self.options['verbose']:
+        print('unused: %d' % numrows)
+        print('randrow: %d' % randrow)
+      idx = 0
+      for row in cursor:
+        if idx == randrow:
+          return row
+        idx += 1
+    return None
+
+  def __get_least_used(self, cursor):
+
+    query = '''SELECT
+                  quotes.quote_id AS qid,
+                  quotes.quote AS quote,
+                  authors.author_name AS author,
+                  categories.category AS category,
+                  quotes.used AS used
+                FROM quotes
+                LEFT JOIN authors ON quotes.author_id = authors.author_id
+                LEFT JOIN categories ON quotes.category_id = categories.category_id
+                ORDER BY quotes.used ASC LIMIT 1;
+                '''
+
+    cursor.execute(query)
+    return cursor.fetchone()
 
   def print_quote(self):
     try:
@@ -253,22 +384,11 @@ class QuoteServer():
 
       if not self.options['cron']:
 
-        query = '''SELECT
-                      quotes.quote_id AS qid,
-                      quotes.quote AS quote,
-                      authors.author_name AS author,
-                      categories.category AS category,
-                      quotes.used AS used
-                    FROM quotes
-                    LEFT JOIN authors ON quotes.author_id = authors.author_id
-                    LEFT JOIN categories ON quotes.category_id = categories.category_id
-                    ORDER BY quotes.used ASC LIMIT 1;
-                    '''
-
         cursor = self.dbconn.cursor()
-        cursor.execute(query)
-        row = cursor.fetchone()
 
+        row = self.__get_random_never_used(cursor)
+        if not row:
+          row = self.__get_least_used(cursor)
         if row:
           qid, quote, author, category, used = row
           query = '''UPDATE quotes SET used = used + 1 WHERE quote_id = ?'''
@@ -284,30 +404,41 @@ class QuoteServer():
       self.__add_error(str(e))
 
     for key in self.notices:
-      self.logger.info('%s (%d)', (key, self.notices[key]))
+      self.logger.info('%s (%d)' % (key, self.notices[key]))
     for key in self.warnings:
-      self.logger.warning('%s (%d)', (key, self.warnings[key]))
+      self.logger.warning('%s (%d)' % (key, self.warnings[key]))
     for key in self.errors:
-      self.logger.error('%s (%d)', (key, self.errors[key]))
+      self.logger.error('%s (%d)' % (key, self.errors[key]))
 
 def main():
 
   usage = "usage: %prog [options]"
   parser = OptionParser(usage)
+  parser.add_option("-a", "--author", action="store", dest="author", default=False, help='optional category when parsing')
   parser.add_option("-c", "--cron", action="store_true", dest="cron", default=0, help='specify executed from cron')
+  parser.add_option("-C", "--catagory", action="store", dest="category", default=False, help='optional category when parsing')
   parser.add_option("-d", "--debug", action="store_true", dest="debug", default=0, help='specify debug mode')
+  parser.add_option("-p", "--url", action="store", dest="url", default=False, help='specify parse and add url')
+  parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=0, help='specify verbose mode')
   parser.add_option("-V", "--version", action="store_true", dest="version", default=False, help='show version and exit')
 
   (opts, args) = parser.parse_args()
 
   options = vars(opts)
 
+  if options['debug']:
+    print('opts: %s' % opts)
+    print('args: %s' % args)
+
   if options['version']:
     print('%s Version: 3.0.0' % Path(sys.argv[0]).name)
     exit(0)
 
   qs = QuoteServer(options)
-  qs.print_quote()
+  if opts.url:
+    qs.parse_url(opts.url, opts.author, opts.category)
+  else:
+    qs.print_quote()
 
 if __name__ == '__main__':
   main()
